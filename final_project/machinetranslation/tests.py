e091dc8eb1048a6113b58e935ae2670f3375cfad
boonewr <boonewr@guilford.edu>
2023-06-01 08:48:06 -0400

    Final touches
    Dockerfile

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,8 +5,14 @@ class TestTranslator(unittest.TestCase):
     def test_englishToFrench(self):
         self.assertEqual(englishToFrench("Hello"), "Bonjour")
 
+    def test_englishToFrench2(self):
+        self.assertNotEqual(englishToFrench("Hello"), "Hello")
+
     def test_frenchToEnglish(self):
         self.assertEqual(frenchToEnglish("Bonjour"), "Hello")
+
+    def test_frenchToEnglish2(self):
+        self.assertNotEqual(frenchToEnglish("Bonjour"), "Bonjour")
 
 if __name__ == '__main__':
     unittest.main()
